eafd88622e61a0b5553e2622ed3d3d99a82855a7
marcodem11 <marcodemicheleta01@outlook.it>
2022-02-28 09:15:55 +0100

    ;

---
+++ app.js
@@ -0,0 +1,24 @@
+const app = new Vue({
+    el: '#app',
+  
+    data:{
+      
+      newItem: '',
+      listItem: [
+        {
+          text: 'Buttare la spazzatura',
+          done: 'false'
+        },
+        {
+          text: 'Svegliarsi presto',
+          done: 'false'
+        },
+        {
+          text: 'Allenarsi costantemente',
+          done: 'false'
+        },
+      ]
+    },
+  
+    
+  })
